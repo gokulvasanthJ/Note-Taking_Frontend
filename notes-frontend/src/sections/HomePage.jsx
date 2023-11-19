d91edb332033a1030214d361b93f561c56c2661f
gokulvasanthJ <gokul27mech@gmail.com>
2023-11-19 21:49:17 +0530

    img comit

--- notes-frontend/src/sections/HomePage.jsx
+++ notes-frontend/src/sections/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
       >
     <Box 
      height={"100vh"}
-     backgroundImage={"https://user-images.githubusercontent.com/101392591/193494962-789f5443-5e7e-429b-aecd-b612ec937c6b.jpg"}
+     backgroundImage={"https://img.freepik.com/free-photo/top-view-paper-woman-sitting-notebook_23-2149001154.jpg?w=900&t=st=1700410731~exp=1700411331~hmac=17312d1fee1c64a66896ced2ee83573a752f4b86f3abff3b59011c3e864fe17f"}
      backgroundSize="cover"
      overflowY="hidden">
         <Button size={"lg"} colorScheme="blackAlpha" mt={"5%"} color="blackAlpha.800" pt={"10px"} pb="20px" fontSize="4xl">The Notes App</Button>
